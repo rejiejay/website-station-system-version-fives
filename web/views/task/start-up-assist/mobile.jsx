55368b52f62a636c731cab2520f9bb0ad2e2c3f8
rejiejay <454766952@qq.com>
2020-06-14 21:50:06 +0800

    add start-up-assist

--- web/views/task/start-up-assist/mobile.jsx
+++ web/views/task/start-up-assist/mobile.jsx
@@ -1,17 +1,28 @@
+import CONST from './const.js'
+
 export default class MobileComponent extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            content: CONST.START_UP_ASSIST.DEFAULT
+        }
     }
 
     async componentDidMount() {
     }
 
     render() {
-        return [
-            <div>记录系统</div>
-        ]
+        const { content } = this.state
+
+        return <div className="mobile">
+            {content.map((value, key) => [
+                <div className="spiritual-title" key={key}>{value.title}</div>,
+                value.item.map((item, id) => <div className="spiritual-item" key={id}>
+                    <div className="item-container">{item}</div>
+                </div>)
+            ])}
+        </div>
     }
 }
 
--- web/views/task/start-up-assist/windows.jsx
+++ web/views/task/start-up-assist/windows.jsx
@@ -1,17 +1,47 @@
+import CONST from './const.js'
+
 export default class WindowsComponent extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            isEdit: false,
+            content: CONST.START_UP_ASSIST.DEFAULT
+        }
     }
 
     async componentDidMount() {
     }
 
     render() {
-        return [
-            <div>记录系统</div>
-        ]
+        const { content, isEdit } = this.state
+
+        return <div class="windows flex-column-center">
+
+            {isEdit && <textarea class="flex-rest" rows="12" cols="20"></textarea>}
+            {!isEdit && <div className="windows-content flex-rest">
+                {content.map((value, key) => [
+                    <div className="windows-title" key={key}>{value.title}</div>,
+                    value.item.map((item, id) => <div className="windows-item" key={id}>
+                        <div className="item-container">· {item}</div>
+                    </div>)
+                ])}
+            </div>}
+
+            <div class="button">
+                <div class="flex-start-center">
+                    <span></span>
+                    <button class="button-item flex-rest">暂存</button>
+                    <span></span>
+                    <button class="button-item flex-rest">查看JSON</button>
+                    <span></span>
+                    <button class="button-item flex-rest">格式化</button>
+                    <span></span>
+                    <button class="button-item flex-rest">恢复</button>
+                    <span></span>
+                </div>
+            </div>
+        </div>
     }
 }
 
