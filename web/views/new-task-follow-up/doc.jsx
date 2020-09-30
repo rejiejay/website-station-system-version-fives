0bd0862da33e33a89b9cd1eca3d6129c685804b8
rejiejay <454766952@qq.com>
2020-09-30 21:29:50 +0800

    add new-task-follow-up doc

---
+++ web/views/new-task-follow-up/doc.jsx
@@ -0,0 +1,30 @@
+tartget = [
+    'for More Simpler to use'
+]
+
+requirement = {
+    Data_persistence: 'for More Simpler to use',
+
+    TargetMindList_List: 'for default list all',
+    TargetMindList_List_sort_random: 'for select task to do',
+    popOutTargetMindSelect: 'for user select target mind',
+    TargetMindList_Mind: 'for target select',
+    switchTargetMindList: 'for switch all list and mind target select',
+    switchDetailMindList: 'for switch target mind list and mind detail select',
+    TargetMindList_MindAdd: 'for add TargetMind',
+    TaskDetail: 'for Add; edit; delete; complete;',
+    TaskDetail_PutOff: '（Medium） for can`t do task record and make clear Task list',
+
+    TaskDetail_Link: '（unimportant） for link task in mind',
+    popOutTip: '（unimportant）for tip how to do',
+    popOutStatistics: '（unimportant）for view Completed tasks',
+}
+
+TaskList_requirement = () => {
+    config = {
+        sortBtn: 'for switch view task way',
+        addBtn: 'for add task',
+        editBtn: 'for edit task',
+        item: 'for view item'
+    }
+}
