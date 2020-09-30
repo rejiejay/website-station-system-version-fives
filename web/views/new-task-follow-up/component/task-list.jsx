42d08bc40f1c73e83d8369b0ce8faa0433ea1a17
rejiejay <454766952@qq.com>
2020-10-01 07:29:10 +0800

    update task list

--- web/views/new-task-follow-up/component/task-list.jsx
+++ web/views/new-task-follow-up/component/task-list.jsx
@@ -1,10 +1,47 @@
+import CONST from './../const.js';
+import utils from './../utils.js';
+
 export default class TaskList extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { }
+        this.state = {
+            pageStatus: CONST.TASK_LIST_STATUS.DEFAULT, // for control page show, because hiden; show all; show group
+            allTaskList: [], // for persistence all list, because have group list
+            allTaskPageNo: 1,
+            allTaskCount: 0,
+            groupTaskList: [], // for all group list, no Pagination, just show all
+        }
+    }
+
+    async showAll() {
+        let { isInitAllTask, state = { isShow: 'showAll' } } = utils.getTaskListShowAllData(this)
+        if (!isInitAllTask) {
+            // const fetchInstance = await fetch.get('allTaskList', { pageNo: 1 })
+            // if (fetchInstance.result !== 1) return reGetConfirm(fetchtInstance.message, this.showAll)
+            // state.allTaskList = fetchInstance.data.allTaskList
+            // state.allTaskCount = fetchInstance.data.allTaskCount
+        }
+        this.setState(state)
     }
 
     render() {
-        return <></>
+        const { isShow, pageStatus, allTaskList, groupTaskList } = this.state
+        const { sort, switchSortHandle, isShowPutOff, switchShowPutOff, editHandle, addHandle } = this.props
+        const { TaskListItem, PutOffButton } = this.props.children
+        const taskListData = utils.getTaskRenderList({ pageStatus, allTaskList, groupTaskList, isShowPutOff })
+
+        return <div className="task-list" style={utils.renderTaskListStyle(isShow)}>
+            <PutOffButton status={isShowPutOff} handle={switchShowPutOff} />
+            <div className="task-list-item">{taskListData.map((task, key) =>
+                <TaskListItem key={key}
+                    edit={editHandle}
+                    data={task}
+                />
+            )}</div>
+            <div className="task-list-operation">
+                <div className="operation-sort" onClick={switchSortHandle}>{sort}</div>
+                <div className="operation-add" onClick={addHandle}>add</div>
+            </div>
+        </div>
     }
 }
