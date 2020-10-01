e473fcd5c8aa79b9eda24de3d79bfb0ae864931f
rejiejay <454766952@qq.com>
2020-10-02 00:22:24 +0800

    add addNewRootMind

--- web/views/new-task-follow-up/component/target-mind-list-select-modal.jsx
+++ web/views/new-task-follow-up/component/target-mind-list-select-modal.jsx
@@ -1,3 +1,7 @@
+import inputPopUp from './../../../components/input-popup.jsx';
+
+import Server from './../server.js';
+
 export default class TargetMindListSelectModal extends React.Component {
     constructor(props) {
         super(props)
@@ -7,6 +11,17 @@ export default class TargetMindListSelectModal extends React.Component {
     }
 
     async show() { }
+
+    async addNewRootMind(reGetRootMindName) {
+        const { taskMindListData } = this.state
+
+        if (!reGetRootMindName) this.rootMindName = await inputPopUp()
+        const rootMindName = reGetRootMindName ? reGetRootMindName : this.rootMindName
+        const fetchInstance = await Server.addNewRootMind({ rootMindName })
+        // if (fetchInstance.result !== 1) return reGetConfirm(fetchtInstance.message, () => this.addNewRootMind(rootMindName))
+        // const taskMind = fetchInstance.data
+        // this.setState({ taskMindListData: [taskMind, ...taskMindListData] })
+    }
 
     render() {
         const { taskMindListData } = this.state
@@ -19,6 +34,9 @@ export default class TargetMindListSelectModal extends React.Component {
         >
             <div className="target-mind-list-select-modal">
                 <PutOffButton status={isShowPutOff} handle={switchShowPutOff} />
+                <div className="target-mind-add"
+                    onClick={this.addNewRootMind.bind(this)}
+                >新增Mind</div>
                 <div className="task-mind-item">{taskMindListData.map((mind, key) =>
                     <TaskMindItem key={key}
                         click={() => switchShow({ showTaskWay: 'mindDetailSelect', groupId: mind.rooId })}
@@ -28,10 +46,10 @@ export default class TargetMindListSelectModal extends React.Component {
                 <ListOperation
                     leftButtonFun={() => switchShow({ showTaskWay: 'listAll' })}
                     leftButtonDes={'任务列表'}
-                    rightOperation={[ { name: 'add', fun: addHandle } ]}
+                    rightOperation={[{ name: 'add', fun: addHandle }]}
                 />
             </div>
         </Modal>
-    
+
     }
 }
--- web/views/new-task-follow-up/server.js
+++ web/views/new-task-follow-up/server.js
@@ -1,5 +1,6 @@
 const Server = {
-    addTask: async({ task, taskRootId }) => {}
+    addTask: async({ task, taskRootId }) => {},
+    addNewRootMind: async({ task, taskRootId }) => {},
 }
 
 export default Server
