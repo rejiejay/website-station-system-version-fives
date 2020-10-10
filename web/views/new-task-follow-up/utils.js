2c4f1ccfb29c607755a0a12b0a47ada18727797e
rejiejay <454766952@qq.com>
2020-10-10 22:59:18 +0800

    update utils

--- web/views/new-task-follow-up/utils.js
+++ web/views/new-task-follow-up/utils.js
@@ -1,48 +1,71 @@
 import uiStorage from './../../components/ui-storage/index';
 
-const utils = {
-    initLayoutRef: self => {
-        self.taskListRef = React.createRef()
-        self.taskDetailModalRef = React.createRef()
-        self.targetMindListSelectModalRef = React.createRef()
-        self.targetMindDetailSelectModalRef = React.createRef()
-        self.tipModalRef = React.createRef()
-        self.statisticsModalRef = React.createRef()
-    },
+function initLayoutRef(self) {
+    self.taskListRef = React.createRef()
+    self.taskDetailModalRef = React.createRef()
+    self.targetMindListSelectModalRef = React.createRef()
+    self.targetMindDetailSelectModalRef = React.createRef()
+    self.tipModalRef = React.createRef()
+    self.statisticsModalRef = React.createRef()
+}
 
-    getShowTaskWayStorage: ({ defaultValue }) => {
-        const showTaskWay = uiStorage.getShowTaskWay()
-        if (showTaskWay) return showTaskWay
-        return defaultValue
-    },
-    getListSortStorage: ({ defaultValue }) => {
-        const sort = uiStorage.getTaskListSort()
-        if (sort) return sort
-        return defaultValue
-    },
-    getMTGTRidStorage: () => {
-        const mindTargetGroupTaskRootId = uiStorage.getMTGTRid()
-        if (mindTargetGroupTaskRootId) return mindTargetGroupTaskRootId
-        return null
-    },
-    storageShowTaskWay: ({ showTaskWay, groupId, self }) => {
-        self.showTaskWay = showTaskWay
-        self.mindTargetGroupTaskRootId = groupId
-        uiStorage.setShowTaskWay({ showTaskWay, groupId })
-    },
+function getShowTaskWayStorage({ defaultValue }) {
+    const showTaskWay = uiStorage.getShowTaskWay()
+    if (showTaskWay) return showTaskWay
+    return defaultValue
+}
+
+function getListSortStorage({ defaultValue }) {
+    const sort = uiStorage.getTaskListSort()
+    if (sort) return sort
+    return defaultValue
+}
+
+function getMTGTRidStorage() {
+    const mindTargetGroupTaskRootId = uiStorage.getMTGTRid()
+    if (mindTargetGroupTaskRootId) return mindTargetGroupTaskRootId
+    return null
+}
+
+function storageShowTaskWay({ showTaskWay, groupId, self }) {
+    self.showTaskWay = showTaskWay
+    self.mindTargetGroupTaskRootId = groupId
+    uiStorage.setShowTaskWay({ showTaskWay, groupId })
+}
+
+function getTaskRenderList({ pageStatus, allTaskList, groupTaskList, isShowPutOff }) {
+    if (pageStatus === 'showGroup') return groupTaskList
+    return allTaskList
+}
+
+function getTaskListLeftOperation(pageStatus, leftButtonFun) {
+    if (pageStatus === 'showGroup') return leftButtonFun
+    return false
+}
+
+const utils = {
+    initLayoutRef,
+
+    getShowTaskWayStorage,
+
+    getListSortStorage,
+
+    getMTGTRidStorage,
+
+    storageShowTaskWay,
+
     renderTaskListStyle: isShow => ({ display: isShow ? 'block' : 'none' }),
-    getTaskRenderList: ({ pageStatus, allTaskList, groupTaskList, isShowPutOff }) => {
-        if (pageStatus === 'showGroup') return groupTaskList
-        return allTaskList
-    },
+
+    getTaskRenderList,
+
     getPutOffStorage: () => {},
+
     // for persistence, because today just need get one time
     getTodayGroupTaskRootId: async() => {},
+
     isAddTaskToday: showTaskWay => {},
-    getTaskListLeftOperation: (pageStatus, leftButtonFun) => {
-        if (pageStatus === 'showGroup') return leftButtonFun
-        return false
-    }
+
+    getTaskListLeftOperation
 }
 
 export default utils
