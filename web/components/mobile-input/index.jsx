86ed9b559387c24d6b9709068ba3f72d44bcf7f7
rejiejay <454766952@qq.com>
2020-11-04 23:13:08 +0800

    update mobile-input

--- web/components/mobile-input/index.jsx
+++ web/components/mobile-input/index.jsx
@@ -4,37 +4,40 @@ class Utils extends React.Component {
     constructor(props) {
         super(props)
     }
-
-    renderRightTopDiv() {
-        const { children } = this.props
-        return ''
-    }
 }
 
-export default class TaskDetailModal extends Utils {
+export default class MobileInput extends Utils {
     constructor(props) {
         super(props)
-        this.state = {
-        }
     }
 
     render() {
-        const { title, value, onChangeHandle, placeholder, isRequiredHighlight, isAutoHeight, height } = this.props
+        const { title, value, onChangeHandle, placeholder, isRequiredHighlight, isAutoHeight, height, isTheme, children } = this.props
 
         return <div className="mobile-input">
-            <div className="input-title flex-start">
-                <div className="input-title flex-rest"
-                    style={style.inputTitleStyle(isRequiredHighlight)}
-                >{isRequiredHighlight && '*'}{title}</div>
-                {this.renderRightTopDiv()}
+            <div className="input-title flex-start" style={style.inputTitleStyle(isRequiredHighlight)}>
+                <div className="input-title flex-rest">{isRequiredHighlight && '*'}{title}</div>
+
+                {children && children.rightTopDiv}
+
             </div>
-            <div className="input-area">
-                <textarea type="text"
-                    style={style.inputTextarea(height, isAutoHeight, value)}
-                    value={value}
-                    onChange={({ target: { value } }) => onChangeHandle(value)}
-                    placeholder={placeholder}
-                />
+            <div className="input-area flex-center" style={style.inputArea}>
+                {isTheme &&
+                    <input type="text"
+                        style={style.inputTheme()}
+                        value={value}
+                        onChange={({ target: { value } }) => onChangeHandle(value)}
+                        placeholder={placeholder}
+                    />
+                }
+                {!isTheme &&
+                    <textarea type="text"
+                        style={style.inputTextarea(height, isAutoHeight, value)}
+                        value={value}
+                        onChange={({ target: { value } }) => onChangeHandle(value)}
+                        placeholder={placeholder}
+                    />
+                }
             </div>
         </div>
     }
